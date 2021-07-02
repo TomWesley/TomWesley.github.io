9c503eb2dccc94e636ad3cea1ec245423d580c0e
Thomas Wesley <tjwesley@crimson.ua.edu>
2021-07-01 23:46:21 -0400

    Porting Terrain

--- sketch.js
+++ sketch.js
@@ -6,71 +6,107 @@ let backgroundCount = 0;
 let wave = 5;
 var song;
 
+let cols, rows;
+let scl = 20;
+let telemetry =[];
+//int w =5000;
+let w = 5000;
+//int h = 8200;
+let h = 7000;
+let delay = 0 ;
+let opac = 255;
+let flying = 0;
+let terrain = [][];
+//let chamber =0;
 function preload(){
   song = loadSound('CarlSagan.mp3');
 }
 
 function setup() {
-  createCanvas(640, 360);
+  telemetry[0]=color(255, 110, 150, opac);
+telemetry[1] =color(255, 200, 10, opac);
+telemetry[2] =color(43, 88, 222,opac);
+telemetry[3] =color(53, 200, 154,opac);
+//telemetry[4] =color(0,0,0,opac);
+telemetry[4] =color(255,255,255,opac);
+
+  //fullScreen(P3D);
+  size(1080, 1350, P3D);
+  cols = (w / scl);
+  rows = (h/ scl);
+  //terrain = new float[cols][rows];
 }
 
 function draw() {
-  translate(width/2, height/2);
+  delay = delay +1;
+  //delay = delay +1;
+  flying -= .1;
+
+  let yoff = flying;
+  for (let y = 125; y < rows; y++) {
+    let xoff = 0;
+    for (let x = 0; x < cols; x++) {
+     /* if(delay > 150 && x < cols/2+(delay-150)*.01 && x > cols/2-(delay-150)*.01){
+        terrain[x][y] = 0;
+      }else{*/
+      terrain[x][y] = map(noise(xoff, yoff), 0, 1, 0, delay);
+     // }
+      xoff += 0.1;
+    }
+    yoff += 0.1;
+  }
+
+
+
+  background(0+40*(1+tan(delay*PI/50)),0,0+70*(1+sin(delay*PI/50)));
   noStroke();
-  backgroundCount = backgroundCount + 1;
-  let temporary = radians(backgroundCount);
- fill((200 * abs(x(temporary))),(20 * abs(sin(temporary))),200 * abs(cos(temporary)),25);
-  //fill(0,20);
-  rect(-width/2, -height/2, width, height);
-
-
-  if (keyIsDown(LEFT_ARROW)) {
-    rotate(radians(-delay));
+  //stroke(255);
+  noFill();
+  let topValue;
+  /*if(delay%5 == 0){
+    topValue = 1;
   }
-
-  if (keyIsDown(RIGHT_ARROW)) {
-    rotate(radians(delay));
+  else if(delay % 6 == 0){
+    topValue = 3;
   }
+  else{
+    topValue = 5;
+  }*/
+  topValue = 1;
+  translate(width/2, height/2+250);
+  rotateX(PI/2.2);
+  translate(-w/2, -h/2);
+  for (let y = 125; y < rows-1; y++) {
+    rotateX(y*PI/cols);
+    if(chamber >= topValue){
+      chamber = 0;
+    }
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < cols; x++) {
 
-  delay=delay+1;
+      //fill(255-0*terrain[x][y]/200,200-120*terrain[x][y]/200,10-10*terrain[x][y]/200,255);
+     // if(delay>100){
+     // stroke(240, 110, 150, 255*y/410);
+    //  }
 
+    stroke(telemetry[chamber]);
+      //stroke(240, 110, 150, 155+50*(1+cos(delay*PI/10)));
+      //fill(43,88,222,255*y/410);
+      fill(255);
+      noFill();
+      vertex(x*scl, y*scl, terrain[x][y]);
+      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
 
-	let refx = 0;
-	let refy = 0;
-	let theta = 0;
-    let phase = 0;
-	let meh = 0;
-	let osx = 0;
-	let osy = 0;
-    if(delay % 360 == 0){
-      wave = int(random(3,25));
+      //rect(x*scl, y*scl, scl, scl);
+
     }
 
-	let rad = 360;
-	let radius = .8*height*sin(radians(delay))/(PI);
-	for (let i = 0; i < rad; i = i + 1) {
-
-	  theta = i * (360 / rad);
-	  phase = ((PI) / rad);
-	  meh = (radius * 1.5 + 11.5) * sin(wave * theta + phase) * cos(phase);
-	  osx = (width / 25 + meh) * cos(theta);
-	  osy = (width / 25 + meh) * sin(theta);
-      stroke(255, 4);
-	  strokeWeight(5);
-	  point(osx + refx, osy + refy);
-      stroke(255, 7);
-	  strokeWeight(4);
-	  point(osx + refx, osy + refy);
-      stroke(255, 100);
-	  strokeWeight(2);
-	  point(osx + refx, osy + refy);
-	  stroke(255, 255);
-	  strokeWeight(1);
-	  point(osx + refx, osy + refy);
-						}
-
-   translate(-width/2, -height/2);
+    endShape();
+     chamber = chamber+1;
+    rotateX(-y*PI/cols);
+  }
 }
+
 
 function mouseClicked() {
   if(song.isPlaying()){
